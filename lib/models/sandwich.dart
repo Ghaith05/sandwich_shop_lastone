518b07cae3f0f5c4b44d1fa0c3222c7a17065377
Ghaith05 <ghaithahmadalsawair73@gmail.com>
2025-11-09 18:20:08 +0000

    update code in sandwich.dart

--- lib/models/sandwich.dart
+++ lib/models/sandwich.dart
@@ -1,44 +1,43 @@
 enum BreadType { white, wheat, wholemeal }
 
-enum SandwichType {
-  veggieDelight,
-  chickenTeriyaki,
-  tunaMelt,
-  meatballMarinara,
-}
+enum SandwichSize { footlong, sixInch }
 
 class Sandwich {
-  final SandwichType type;
-  final bool isFootlong;
+  final String id;
+  final String name;
+  final String description;
+  final bool available;
   final BreadType breadType;
+  final SandwichSize size;
+  final String image;
 
   Sandwich({
-    required this.type,
-    required this.isFootlong,
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.available,
     required this.breadType,
+    required this.size,
+    required this.image,
   });
 
-  String get name {
-    switch (type) {
-      case SandwichType.veggieDelight:
-        return 'Veggie Delight';
-      case SandwichType.chickenTeriyaki:
-        return 'Chicken Teriyaki';
-      case SandwichType.tunaMelt:
-        return 'Tuna Melt';
-      case SandwichType.meatballMarinara:
-        return 'Meatball Marinara';
-    }
+  factory Sandwich.fromJson(Map<String, dynamic> json) {
+    return Sandwich(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      description: json['description'] as String? ?? '',
+      available: json['available'] as bool? ?? true,
+      breadType: BreadType.values.firstWhere(
+        (e) => e.name == (json['breadType'] as String? ?? 'white'),
+        orElse: () => BreadType.white,
+      ),
+      size: SandwichSize.values.firstWhere(
+        (e) => e.name == (json['size'] as String? ?? 'footlong'),
+        orElse: () => SandwichSize.footlong,
+      ),
+      image: json['image'] as String? ?? '',
+    );
   }
 
-  String get image {
-    String typeString = type.name;
-    String sizeString = '';
-    if (isFootlong) {
-      sizeString = 'footlong';
-    } else {
-      sizeString = 'six_inch';
-    }
-    return 'assets/images/${typeString}_$sizeString.png';
-  }
+  bool? get isFootlong => null;
 }
